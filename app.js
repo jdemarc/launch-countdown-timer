0721255f5a30f9a088497e24771f729d0fca88bc
joseph <josephjohndemarco@gmail.com>
2021-01-18 19:22:57 -0500

    Add countdown logic.

---
+++ app.js
@@ -0,0 +1,17 @@
+const countdown = new Date("February 1, 2021 12:00:00").getTime();
+
+let x = setInterval(function() {
+  let now = new Date().getTime();
+
+  let distance = countdown - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  timeEl = document.getElementById('time');
+  timeEl.innerHTML = days + ' ' + hours + ' ' + minutes + ' ' + seconds;
+
+  distance < 0 ? clearInterval(x) : timeEl = 'EXPIRED';
+}, 1000);
